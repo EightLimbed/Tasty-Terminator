c3a08ea4bb495514b4b46e9966ee0655f9471451
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-09-20 14:45:16 -0400

    Initial commit

---
+++ World/world_resource.gd
@@ -0,0 +1,22 @@
+extends Resource
+
+class_name World
+
+#tiles to choose from for background
+@export var main_tiles : TileSet
+
+#tiles to choose from for roads
+@export var road_tiles : TileSet
+@export var length_range : Vector2i
+@export var branching_chance : int
+
+#tiles to choose from for building, y is roof, middle, bottom, x is kinds of things for each (do 3)
+@export var building_tiles : TileSet
+@export var building_height : int
+#specifies whether building parts can mix
+@export var shuffle_top : bool
+@export var shuffle_middle : bool
+@export var shuffle_bottom : bool
+
+#powerup textures
+@export var crate_textures : SpriteFrames
