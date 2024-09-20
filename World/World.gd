e4c4cd1d656e63fe2120ad4ba1240da6b7489b18
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-09-20 15:16:15 -0400

    Road Generation Start

--- World/World.gd
+++ World/World.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var visited_roads : Array = []
+@onready var roads = $Roads
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func generate_roads(pos : Vector2i, size : Vector2i):
+	for x in size.x:
+		for y in size.y:
+			var updated_pos = pos-(size/2)+Vector2i(x,y)
+			if not visited_roads.has(updated_pos):
+				pass
+				#set cell using autotiling
