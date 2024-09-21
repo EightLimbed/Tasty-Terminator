5e803f30ee2c7b3d13fcffb41a9a21e9e13018fd
Dryden <drydenellis@gmail.com>
2024-09-20 22:07:36 -0400

    Added Test Road Texture

--- World/World.gd
+++ World/World.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
+var profile = preload("res://World/NewYork.tres")
+
 var visited_roads : Array = []
 @onready var roads = $Roads
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#roads.tile_set = profile.road_tiles
+	generate_roads(Vector2i(0,0), Vector2i(64, 64))
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+#remember to use local_to_map for position
 func generate_roads(pos : Vector2i, size : Vector2i):
 	for x in size.x:
 		for y in size.y:
 			var updated_pos = pos-(size/2)+Vector2i(x,y)
 			if not visited_roads.has(updated_pos):
+				#use get_surounding_cells() or get_neighbor_cell() for rules
+
 				pass
-				#set cell using autotiling
