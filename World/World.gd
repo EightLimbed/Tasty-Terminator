5faf6aa3593cf58a5e9669a91fbe57d796a1464a
Dryden <drydenellis@gmail.com>
2024-09-21 22:13:42 -0400

    Fiddling with Roads

--- World/World.gd
+++ World/World.gd
@@ -2,22 +2,23 @@ extends Node2D
 
 var profile = preload("res://World/NewYork.tres")
 
-var visited_roads : Array = []
+var roads_done : Array = []
+var intersections : Array = []
+
+var random = RandomNumberGenerator.new()
 @onready var roads = $Roads
 
 func _ready():
-	#roads.tile_set = profile.road_tiles
 	generate_roads(Vector2i(0,0), Vector2i(64, 64))
+	#make_line(Vector2i(0,0), Vector2(12,10))
+	#roads.tile_set = profile.road_tiles
+	pass
 
 func _process(delta):
+	#generate_roads(Vector2i(0,0), Vector2i(512, 256))
 	pass
 
 #remember to use local_to_map for position
 func generate_roads(pos : Vector2i, size : Vector2i):
-	for x in size.x:
-		for y in size.y:
-			var updated_pos = pos-(size/2)+Vector2i(x,y)
-			if not visited_roads.has(updated_pos):
-				#use get_surounding_cells() or get_neighbor_cell() for rules
-
-				pass
+	#just make grid, but warp it, and connect points for roads
+	pass
