51de1ca242aedfbeb0e80881f7dc074eceba49cd
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-09-24 13:50:53 -0400

    Road Tweaking

--- World/World.gd
+++ World/World.gd
@@ -7,10 +7,10 @@ var random = RandomNumberGenerator.new()
 @onready var roads = $Roads
 
 func _ready():
-	noise.noise_type = FastNoiseLite.TYPE_VALUE_CUBIC
-	noise.frequency = 0.15
+	noise.noise_type = FastNoiseLite.TYPE_VALUE
+	noise.frequency = 0.2
 	noise.fractal_type = FastNoiseLite.FRACTAL_RIDGED
-
+	noise.fractal_gain = 0.01
 	noise.seed = random.randi()
 
 #roads
@@ -19,7 +19,7 @@ func generate_roads(pos : Vector2i, size : Vector2i):
 		for y in size.y:
 			var updated_pos = roads.local_to_map(pos)-size/2+Vector2i(x,y)
 			var alt : int = round_noise(updated_pos)
-			var surrounding = [round_noise(updated_pos+Vector2i(1,1)), round_noise(updated_pos+Vector2i(-1,1)), round_noise(updated_pos+Vector2i(-1,-1)), round_noise(updated_pos+Vector2i(1,-1)), round_noise(updated_pos+Vector2i(1,0)), round_noise(updated_pos+Vector2i(0,1)), round_noise(updated_pos+Vector2i(-1,0)), round_noise(updated_pos+Vector2i(0,-1))]
+			var surrounding = [round_noise(updated_pos+Vector2i(1,1)), round_noise(updated_pos+Vector2i(-1,1)), round_noise(updated_pos+Vector2i(-1,-1)), round_noise(updated_pos+Vector2i(1,-1))]
 
 			if alt == 1 and surrounding.has(0):
 				var atlas = Vector2i(1,1)
@@ -32,7 +32,8 @@ func generate_roads(pos : Vector2i, size : Vector2i):
 				if not roads_done.has(updated_pos+Vector2i(0,-1)):
 					atlas.y -= 1
 				roads.set_cell(updated_pos, 0, atlas)
-				roads_done.append(updated_pos)
+				if not roads_done.has(updated_pos):
+					roads_done.append(updated_pos)
 
 func round_noise(pos : Vector2i):
 	var alt : int = round(noise.get_noise_2d(pos.x, pos.y))
