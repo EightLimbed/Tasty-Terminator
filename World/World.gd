a36e30ca88cd5314d02c2ec4cfa5c035bd000e7f
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-09-25 13:53:39 -0400

    New Road Generation
    perfect, except for roads tweaking, and one of diagonals not working properly

--- World/World.gd
+++ World/World.gd
@@ -1,32 +1,26 @@
 extends Node2D
 
-var noise = FastNoiseLite.new()
 var roads_cache : Array = []
 
 var random = RandomNumberGenerator.new()
 @onready var roads = $Roads
+@onready var noise = $Noise
 
 func _ready():
-	noise.noise_type = FastNoiseLite.TYPE_CELLULAR
-	noise.frequency = 0.05
-	noise.fractal_type = FastNoiseLite.FRACTAL_RIDGED
-	noise.cellular_distance_function = FastNoiseLite.DISTANCE_MANHATTAN
-	noise.cellular_return_type = FastNoiseLite.RETURN_CELL_VALUE
-	noise.cellular_jitter = 0.5
-	noise.seed = random.randi()
+	pass
 
 #roads
 func generate_roads(pos : Vector2i, size : Vector2i, cache_size : int):
-	if roads_cache.size() > cache_size:
-		roads_cache.slice(cache_size/2, cache_size)
+	#noise.position = pos
 	roads.clear()
+	noise.texture.noise.offset = Vector3i(roads.local_to_map(pos).x, roads.local_to_map(pos).y, 0)
+	var image = noise.texture.get_image()
 	for x in size.x:
 		for y in size.y:
 			var updated_pos = roads.local_to_map(pos)-size/2+Vector2i(x,y)
-			var alt : float = round_noise(updated_pos)*2
-			var surrounding : Array = []
+			var alt : Color = image.get_pixel(x,y)
 			#autotile
-			if alt < -0.4:
+			if alt != Color("7f7fff"):
 				var atlas = Vector2i(1,1)
 				if not roads_cache.has(updated_pos+Vector2i(1,0)):
 					atlas.x += 2
@@ -38,9 +32,6 @@ func generate_roads(pos : Vector2i, size : Vector2i, cache_size : int):
 					atlas.y -= 1
 				roads.set_cell(updated_pos, 0, atlas)
 				if not roads_cache.has(updated_pos):
+					#if roads_cache.size() > cache_size:
+						#roads_cache.remove_at(0)
 					roads_cache.append(updated_pos)
-
-func round_noise(pos : Vector2i):
-	var alt : float = (noise.get_noise_2d(pos.x, pos.y))
-	print(alt)
-	return alt
