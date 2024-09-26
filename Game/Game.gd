fd028a1e756f1312932ba4509ce462c0b54f4225
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-09-26 13:40:49 -0400

    Finished Road Generation
    just need tilesets

--- Game/Game.gd
+++ Game/Game.gd
@@ -4,7 +4,8 @@ extends Node2D
 @onready var player = $Player
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#world.profile = load()
+	pass
 
 func _process(_delta: float):
-	world.generate_roads(player.position, Vector2i(24,24), 1152)
+	world.generate_roads(player.position, Vector2i(24,24))
--- World/World.gd
+++ World/World.gd
@@ -3,35 +3,45 @@ extends Node2D
 var roads_cache : Array = []
 
 var random = RandomNumberGenerator.new()
+var noise = FastNoiseLite.new()
 @onready var roads = $Roads
-@onready var noise = $Noise
+@onready var visual_noise = $Noise
 
 func _ready():
-	pass
+	noise.noise_type = FastNoiseLite.TYPE_CELLULAR
+	noise.cellular_jitter = 0
+	#noise.cellular_distance_function = FastNoiseLite.DISTANCE_MANHATTAN
+	noise.frequency = 0.007
+	noise.cellular_return_type = FastNoiseLite.RETURN_CELL_VALUE
+	noise.seed = random.randi()
 
 #roads
-func generate_roads(pos : Vector2i, size : Vector2i, cache_size : int):
+func generate_roads(pos : Vector2i, size : Vector2i):
+	var cache_size = size.x*size.y
 	#noise.position = pos
 	roads.clear()
-	noise.texture.noise.offset = Vector3i(roads.local_to_map(pos).x, roads.local_to_map(pos).y, 0)
-	var image = noise.texture.get_image()
+
 	for x in size.x:
 		for y in size.y:
 			var updated_pos = roads.local_to_map(pos)-size/2+Vector2i(x,y)
-			var alt : Color = image.get_pixel(x,y)
+			var alt : int = round(noise.get_noise_2dv(updated_pos)*10)
+			var surrounding : Array[int] = [round(noise.get_noise_2dv(updated_pos+Vector2i(1,1))*10), 
+											round(noise.get_noise_2dv(updated_pos+Vector2i(-1,1))*10), 
+											round(noise.get_noise_2dv(updated_pos+Vector2i(1,-1))*10), 
+											round(noise.get_noise_2dv(updated_pos+Vector2i(-1,-1))*10)]
 			#autotile
-			if alt != Color("7f7fff"):
+			if surrounding[0] != alt or surrounding[1] != alt or surrounding[2] != alt or surrounding[3] != alt:
 				var atlas = Vector2i(1,1)
 				if not roads_cache.has(updated_pos+Vector2i(1,0)):
-					atlas.x += 2
+					atlas.x += 3
 				if not roads_cache.has(updated_pos+Vector2i(0,1)):
-					atlas.y += 2
+					atlas.y += 3
 				if not roads_cache.has(updated_pos+Vector2i(-1,0)):
 					atlas.x -= 1
 				if not roads_cache.has(updated_pos+Vector2i(0,-1)):
 					atlas.y -= 1
 				roads.set_cell(updated_pos, 0, atlas)
 				if not roads_cache.has(updated_pos):
-					#if roads_cache.size() > cache_size:
-						#roads_cache.remove_at(0)
+					if roads_cache.size() > cache_size:
+						roads_cache.remove_at(0)
 					roads_cache.append(updated_pos)
