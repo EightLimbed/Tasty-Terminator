76e809aa075802ce304f201444ec962787ea3952
DnumdeYevoc <163893629+DnumdeYevoc@users.noreply.github.com>
2024-10-02 14:14:54 -0400

    try agian

--- Game/Game.gd
+++ Game/Game.gd
@@ -11,7 +11,7 @@ var wave : int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
+	
 func _process(_delta: float):
 	world.generate_roads(player.position, Vector2i(24,24))
 
