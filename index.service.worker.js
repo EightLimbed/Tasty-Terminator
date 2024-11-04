87a32a8290549511629a6209b2f51dd727056a45
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-11-04 12:49:52 -0500

    Multitouch Fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730689637|30792076';
+const CACHE_VERSION = '1730742479|128884852';
 /** @type {string} */
 const CACHE_PREFIX = 'Tasty Terminator-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
