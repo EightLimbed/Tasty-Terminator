0556f9f9cf4aa478473f0bb0560341ad20a5279d
Dryden <drydenellis@gmail.com>
2024-11-04 21:48:00 -0500

    Fix Joystick (Actually)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730774526|610089114';
+const CACHE_VERSION = '1730774793|70904160';
 /** @type {string} */
 const CACHE_PREFIX = 'Tasty Terminator-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
