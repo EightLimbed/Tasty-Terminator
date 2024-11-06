178cdac3c5a3f9e8228afae1835b624f46e8fb2c
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2024-11-06 12:59:54 -0500

    Beta Build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730855175|2965875201';
+const CACHE_VERSION = '1730915920|87590701';
 /** @type {string} */
 const CACHE_PREFIX = 'Tasty Terminator-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
