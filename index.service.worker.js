88149a2cdefb1232ca3e15dab4366a2a1ff986ed
Dryden <drydenellis@gmail.com>
2024-11-06 19:19:52 -0500

    Fix Eddy Joystick Mess Up

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730917069|115137249';
+const CACHE_VERSION = '1730938714|912402422';
 /** @type {string} */
 const CACHE_PREFIX = 'Tasty Terminator-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
